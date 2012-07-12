f1905c151388f5f5aac63f2f333741ff4015e4e4
Emily Moss <emilyemoss@gmail.com>
2012-07-12 14:52:02 -0400

    finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,10 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+  
+  # Speed up tests by lowering BCrypt's cost function. - RORT 7.1.4
+ 	require 'bcrypt'
+  silence_warnings do
+  	BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+  end
 end
